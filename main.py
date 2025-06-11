edf695f1e347fe6bf57f72dc4cef8dea12aa70f0
Koivya <ivankovalyov07@gmail.com>
2025-06-11 18:35:03 +0700

    first commit

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def factorial(n: int) -> int:
-    if n == 0:
-        return 1
-    return n * factorial(n - 1)
-
 if __name__ == "__main__":
-    print("Факториал:", factorial(5))
+    print("")
     
