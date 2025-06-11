402c7021e58baf6257c9ee505ea1d55505a72d40
Koivya <ivankovalyov07@gmail.com>
2025-06-11 18:29:21 +0700

    Add factorial function

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def is_prime(n: int) -> bool:
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+def factorial(n: int) -> int:
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
 
 if __name__ == "__main__":
-    print("Сложение:", add(5, 3.5))
-    print("Проверка на простоту:", is_prime(17))
+    print("Факториал:", factorial(5))
     
