668f51f637c6c754c2b275e7fecabc657f766886
Koivya <ivankovalyov07@gmail.com>
2025-06-11 18:24:54 +0700

    oee

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-def is_prime(n: int) -> bool:
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
 
 if __name__ == "__main__":
     print("Сложение:", add(5, 3.5))
