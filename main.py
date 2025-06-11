d51ce11bfcfff11a02e680d9002d9b4d1d71af40
Koivya <ivankovalyov07@gmail.com>
2025-06-11 18:19:24 +0700

    Add is_prime function

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def add(a: float, b: float) -> float:
-    """Сложение двух чисел"""
-    return a + b
+def is_prime(n: int) -> bool:
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 if __name__ == "__main__":
-    # Примеры использования функций
     print("Сложение:", add(5, 3.5))
+    print("Проверка на простоту:", is_prime(17))
     
